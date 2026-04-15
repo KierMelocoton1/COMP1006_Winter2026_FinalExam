ca3ac510455715c4d6e40a8796910f18454f2c1d
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-04-15 15:10:50 -0400

    commit

---
+++ FinalExam/login.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" id="username_or_email" name="user_name_or_email" class="form-control mb-3" required>
+
+</body>
+</html>

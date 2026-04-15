84c15b3c4dea814a568e992086588b046a407a39
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-04-15 14:48:25 -0400

    Add files via upload

---
+++ FinalExam/includes/connect.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$db = "final";
+$user = "root";
+$password = "";
+
+$dsn = "mysql:host=$host;dbname=$db";
+
+try {
+    $pdo new PDO ($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+?>
